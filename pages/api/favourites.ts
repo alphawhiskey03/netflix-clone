2fee04ff6c5efd8c221852ae4e398213aa417bbd
vignesh <vignesh@vigneshs-Air.domain.name>
2023-06-09 00:14:55 +0530

    fix(api): import prismadb

--- pages/api/favourites.ts
+++ pages/api/favourites.ts
@@ -1,5 +1,6 @@
 import serverAuth from "@/lib/serverAuth";
 import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/lib/prismadb";
 
 export default async function handler(
   req: NextApiRequest,
